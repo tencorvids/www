2cc6924b53833582e58eb41b0e33a53c1ad0ee6a
rew <rew@tencorvids.com>
2025-07-12 09:57:43 -0400

    copy

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -6,6 +6,7 @@ import Waves from "$/waves";
 import { Button } from "@tencorvids/ui";
 import { TransitionPanel } from "$/transition-panel";
 import { cx } from "@tencorvids/util";
+import { GithubLogoIcon } from "@phosphor-icons/react";
 
 export default function Page() {
     return (
@@ -13,10 +14,11 @@ export default function Page() {
             <GridSection />
 
             <GridSection className="p-4">
-                <h2 className="text-xs text-muted-foreground">tencorvids</h2>
-                <h1 className="text-muted-foreground mt-1">And<span className="text-brand font-medium">[rew]</span> Vota</h1>
+                <h2 className="text-xs text-muted-foreground font-mono">tencorvids</h2>
+                <h1 className="mt-1">And<span className="text-brand font-medium">[rew]</span> Vota</h1>
+                <h3 className="mt-1 text-muted-foreground">Software Engineer</h3>
 
-                <p className="mt-4">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry"s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                <p className="mt-4 max-w-[80ch]">I'm a Software Engineer building everything from firmware to frontend. Currently, I work on <a href="https://avservices.net/harmony/" target="_blank" className="underline">hardware monitoring systems @ A-V Services</a> <span className="text-muted-foreground italic">(serendipitous name, not my business)</span>, writing firmware and developing internal tools for deployment and management.</p>
             </GridSection>
 
             <GridSection outerClassName="h-32" className="p-4">
@@ -39,16 +41,8 @@ function Panels() {
     const [activeIndex, setActiveIndex] = useState(0);
     const items = [
         {
-            tab: "Writing",
-            panel: <WritingPanel />,
-        },
-        {
             tab: "Projects",
             panel: <ProjectsPanel />,
-        },
-        {
-            tab: "Components",
-            panel: <ComponentsPanel />,
         },
     ];
 
@@ -84,20 +78,68 @@ function Panels() {
     )
 }
 
-function WritingPanel() {
-    return (
-        <p>Writing</p>
-    );
-}
 
 function ProjectsPanel() {
+    const projects = [
+        {
+            title: "twigs",
+            description: "My dotfiles, neovim, tmux, including darwin and linux hosts.",
+            href: "https://github.com/tencorvids/twigs",
+        },
+        {
+            title: "stork",
+            description: "An opinionated template for creating scalable web applications using a pnpm monorepo.",
+            href: "https://github.com/tencorvids/stork",
+        },
+        {
+            title: "rui3-go",
+            description: "Go interface for RAK chips, generally used for LoRa communications on the RUI3-based boards.",
+            href: "https://github.com/tencorvids/rui3-go",
+        },
+        {
+            title: "scrawl",
+            description: "A utility used to generate web fonts for my Magic: The Gathering related projects.",
+            href: "https://github.com/tencorvids/scrawl",
+        },
+        {
+            title: "_",
+            description: "[archived] My first journey into NixOS and learning about declarative environments (fell in love).",
+            href: "https://github.com/tencorvids/_",
+        },
+        {
+            title: "www",
+            description: "You are here.",
+            href: "https://github.com/tencorvids/www",
+        },
+    ];
     return (
-        <p>Projects</p>
+        <div className="space-y-2">
+            {projects.map((project) => (
+                <Row
+                    key={project.title}
+                    href={project.href}
+                    title={project.title}
+                    description={project.description}
+                />
+            ))}
+        </div>
     );
 }
 
-function ComponentsPanel() {
+interface RowProps {
+    href: string;
+    title: string;
+    description: string;
+}
+
+function Row({ href, title, description }: RowProps) {
     return (
-        <p>Components</p>
+        <a href={href} target="_blank" className="w-full flex justify-between items-center gap-2 p-2 rounded hover:bg-accent">
+            <div className="flex gap-2">
+                <p>{title}</p>
+                <p className="text-muted-foreground">{description}</p>
+            </div>
+            <GithubLogoIcon className="size-4 min-w-4 text-muted-foreground" />
+        </a>
     );
 }
